5a6b4ba26d44d9dfca80ce7afee8e14367ae836b
Jimmy Li <jmyli562@gmail.com>
2023-08-31 14:47:52 -0700

    finished lab

--- server/models.py
+++ server/models.py
@@ -1,23 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
-class Zookeeper(db.Model):
-    __tablename__ = 'zookeepers'
-
-    id = db.Column(db.Integer, primary_key=True)
-
-class Enclosure(db.Model):
-    __tablename__ = 'enclosures'
-
-    id = db.Column(db.Integer, primary_key=True)
 
 class Animal(db.Model):
-    __tablename__ = 'animals'
-
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    species = db.Column(db.String(100))
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey("zookeeper.id"))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey("enclosure.id"))
+
+
+class Zookeeper(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    birthday = db.Column(db.Date)
+    animals = db.relationship("Animal", backref="zookeeper", lazy=True)
+
+
+class Enclosure(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(50))
+    open_to_visitors = db.Column(db.Boolean, default=True)
+    animals = db.relationship("Animal", backref="enclosure", lazy=True)
